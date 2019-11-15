1f45580fddd22349d89c4ac56741586870c17a5c
Jon Knight <jon@babble.io>
2019-11-15 14:35:19 +0000

    Initial Stage 3 code

--- app/src/main/java/io/mosaicnetworks/myfirstapp/ChatActivity.java
+++ app/src/main/java/io/mosaicnetworks/myfirstapp/ChatActivity.java
@@ -2,15 +2,71 @@ package io.mosaicnetworks.myfirstapp;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Toast;
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
+public class ChatActivity extends AppCompatActivity implements MessageObserver {
 
-public class ChatActivity extends AppCompatActivity {
+    private MessagesListAdapter<Message> mAdapter;
+    private String mMoniker;
+    private final MessagingService mMessagingService = MessagingService.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        Intent intent = getIntent();
+        mMoniker = intent.getStringExtra("MONIKER");
+
+        initialiseAdapter();
+        mMessagingService.registerObserver(this);
+
+        if (mMessagingService.getState()!=MessagingService.State.RUNNING_WITH_DISCOVERY) {
+            Toast.makeText(this, "Unable to advertise peers", Toast.LENGTH_LONG).show();
+        }
     }
 
+    private void initialiseAdapter() {
+        MessagesList mMessagesList = findViewById(R.id.messagesList);
+
+        mAdapter = new MessagesListAdapter<>(mMoniker, null);
+        mMessagesList.setAdapter(mAdapter);
+
+        MessageInput input = findViewById(R.id.input);
+
+        input.setInputListener(new MessageInput.InputListener() {
+            @Override
+            public boolean onSubmit(CharSequence input) {
+                mMessagingService.submitMessage(new Message(input.toString(), mMoniker));
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onMessageReceived(final Message message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.addToStart(message, true);
+            }
+        });
+    };
+
+    @Override
+    public void onBackPressed() {
+        mMessagingService.stop();
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMessagingService.removeObserver(this);
+
+        super.onDestroy();
+    }
 }
