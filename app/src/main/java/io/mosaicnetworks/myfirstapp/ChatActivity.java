22454d382f973f3ca9f227b79b07093b7964c35b
Jon Knight <jon@babble.io>
2019-11-29 09:57:39 +0000

    Update Stage3 to babble-android 0.2.1

--- app/src/main/java/io/mosaicnetworks/myfirstapp/ChatActivity.java
+++ app/src/main/java/io/mosaicnetworks/myfirstapp/ChatActivity.java
@@ -2,15 +2,79 @@ package io.mosaicnetworks.myfirstapp;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Toast;
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
+
+import io.mosaicnetworks.babble.node.ServiceObserver;
+
+public class ChatActivity extends AppCompatActivity implements ServiceObserver {
 
 
-public class ChatActivity extends AppCompatActivity {
+    private MessagesListAdapter<Message> mAdapter;
+    private String mMoniker;
+    private final MessagingService mMessagingService =
+            MessagingService.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        Intent intent = getIntent();
+        mMoniker = intent.getStringExtra("MONIKER");
+
+        initialiseAdapter();
+        mMessagingService.registerObserver(this);
+
+        if (mMessagingService.getState()!=
+                MessagingService.State.RUNNING_WITH_DISCOVERY) {
+            Toast.makeText(this, "Unable to advertise peers",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
+    private void initialiseAdapter() {
+        MessagesList mMessagesList = findViewById(R.id.messagesList);
+
+        mAdapter = new MessagesListAdapter<>(mMoniker, null);
+        mMessagesList.setAdapter(mAdapter);
+
+        MessageInput input = findViewById(R.id.input);
+
+        input.setInputListener(new MessageInput.InputListener() {
+            @Override
+            public boolean onSubmit(CharSequence input) {
+                mMessagingService.submitTx(new Message(input.toString(), mMoniker).toBabbleTx());
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void stateUpdated() {
+        final Message message = mMessagingService.state.getLatestMessage();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.addToStart(message, true);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        mMessagingService.leave(null);
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMessagingService.removeObserver(this);
+
+        super.onDestroy();
+    }
 }
